0d6864224e1c43a11ca6d4d1ee0ff2497baad647
christie <cbrandao18@gmail.com>
2019-08-13 17:17:45 -0400

    leetcode problem

--- 02_memoization_project/lib/leet_code_518.js
+++ 02_memoization_project/lib/leet_code_518.js
@@ -1,3 +1,17 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+function change(amount, coins, memo={}){
+  let key = amount + '-' + coins;
+  if (key in memo) return memo[key];
+
+  let currCoin = coins[coins.length - 1];
+  let diffWaysForChange = 0;
+
+  for (let i = 0; i * currCoin <= amount; i++){
+    diffWaysForChange += change(amount - i * currCoin, coins.slice(0, -1), memo)
+  }
+
+  memo[key] = diffWaysForChange;
+  return memo[key];
+}
