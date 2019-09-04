98f662bdd8bc8892e5c9bdb8758db1453e980e70
christie <cbrandao18@gmail.com>
2019-09-04 15:39:00 -0400

    add comments to longest palindrome substr problem

--- leetcode/5-longest_palindrome-substr.js
+++ leetcode/5-longest_palindrome-substr.js
@@ -8,14 +8,18 @@ var longestPalindrome = function (s) {
 
     let longest = s[0];
 
+    // initialize matrix that will hold true or false
+    // depending on if the substr from i to j is a palindrome
     let matrix = new Array(s.length);
     for (let row = 0; row < s.length; row++) {
         let col = row;
         matrix[row] = new Array(s.length);
+
+        // all 1 letter substr are palindromes
         matrix[row][col] = true;
     }
 
-    //all two letter substr
+    // fill matrix with all two letter substr
     for (let i = 0; i < s.length; i++) {
         let j = i + 1;
         if (s[i] === s[j]) {
@@ -26,6 +30,8 @@ var longestPalindrome = function (s) {
         }
     }
 
+    // start checking for palindomes of length 3, increasing until the length
+    // of the given str
     for (let len = 3; len <= s.length; len++) {
         for (let start = 0; start <= s.length - len; start++) {
             let end = start + len - 1;
